73ca0a549045ff71d3e12fc87c81f1a2bfd67303
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-02-14 19:04:24 -0500

    Fixed the time delay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,102 +1,101 @@
+$(document).ready(function() {
+  // initialize questions for quiz
+  var questions = [
+    {
+      question: "What is the capital of Nepal? ",
+      possibleAnswers: ["Pokhara", "Kathmandu", "Jomsom", "Delhi"],
+      answer: 1
+    },
 
+    {
+      question: "What is the capital of India? ",
+      possibleAnswers: ["Delhi", "Mumbai", "Banglore", "Chennai"],
+      answer: 0
+    },
 
-$(document).ready (function(){
-
-    var questions = [
-        {
-            question: "What is the capital of Nepal? ",
-            possibleAnswers: ['Pokhara', 'Kathmandu', 'Jomsom', 'Delhi'],
-            answer: 1
-        },
-
-        {
-            question: "What is the capital of India? ",
-            possibleAnswers: ['Delhi', 'Mumbai', 'Banglore', 'Chennai'],
-            answer: 0
-        },
-
-        {
-            question: "What is the capital of USA? ",
-            possibleAnswers: ['New York', 'Boston', 'Washingon DC', 'Maryland'],
-            answer: 2
-        },
-        {
-            question: "What is the capital of Mustang? ",
-            possibleAnswers: ['Pokhara', 'Kathmandu', 'Jomsom', 'Delhi'],
-            answer: 2
-        },
-        
-    ] // end of the questions array
-
-   
-
-
-    var currentQuestion = 0;
-    var totalQuestion = questions.length;
-    var message = document.getElementById('gameMessage');
-    var winLossMessage = document.getElementById('win-loss-message');
-   
-   
-    $("#forms").hide(); // When page loads the forms should be hidden
-
-    function loadQuestion (questionIndex){
-        var index = questions[questionIndex];
-        $('#question').text(index.question);
-        $('#choice1').text(index.possibleAnswers[0]);
-        $('#choice2').text(index.possibleAnswers[1]);
-        $('#choice3').text(index.possibleAnswers[2]);
-        $('#choice4').text(index.possibleAnswers[3]);
+    {
+      question: "What is the capital of USA? ",
+      possibleAnswers: ["New York", "Boston", "Washingon DC", "Maryland"],
+      answer: 2
     }
+  ];
 
-  
+  // get quiz elements
+  var currentQuestion = 0;
+  var totalQuestion = questions.length;
+  var winLossMessage = document.getElementById("win-loss-message");
 
-    
-    function loadNextQuestion (){
+  // helper function for displaying results
+  var hideFormTimeout;
+  function displayResult(rightOrWrong) {
+    console.log(rightOrWrong);
+    $("#forms").hide();
+    winLossMessage.textContent = rightOrWrong;
+    $("#win-loss-message").show();
+    setTimeout(function() {
+      $("#win-loss-message").hide();
+    }, 2000);
+    hideFormTimeout = setTimeout(function() {
+      $("#forms").show();
+    }, 2000);
+  }
 
-        $('input').on('click', function(){
-            var selectedOption = document.querySelector("input[type=radio]:checked")
-            var userChoice = selectedOption.value;
-            console.log("DEBUG userchoice answer's value is " + userChoice);
-            console.log("DEBUG  answer value is " + questions[currentQuestion].answer);
+  $("#forms").hide(); // When page loads the forms should be hidden
+  $("#gameMessage").hide();
 
-            var ans = questions[currentQuestion].answer;
-            if(userChoice == ans){
-                console.log('CORRECT!');
-                // winLossMessage.textContent = "CORRECT";
-                // $("#forms").hide();   
-            }
+  function loadQuestion(questionIndex) {
+    var index = questions[questionIndex];
+    $("#question").text(index.question);
+    $("#choice1").text(index.possibleAnswers[0]);
+    $("#choice2").text(index.possibleAnswers[1]);
+    $("#choice3").text(index.possibleAnswers[2]);
+    $("#choice4").text(index.possibleAnswers[3]);
+  }
 
-            else{
-                console.log("NOPE!");
-                loadQuestion(currentQuestion); 
-            }
+  function loadNextQuestion() {
+    $("input").on("click", function() {
+      var selectedOption = document.querySelector("input[type=radio]:checked");
+      var userChoice = selectedOption.value;
+      console.log("DEBUG userchoice answer's value is " + userChoice);
+      console.log(
+        "DEBUG  answer value is " + questions[currentQuestion].answer
+      );
 
-            // selectedOption = false;
-            currentQuestion++;
+      var ans = questions[currentQuestion].answer;
+      if (userChoice == ans) {
+        displayResult("CORRECT");
+      } else {
+        displayResult("NOPE");
+      }
 
-            if(currentQuestion == totalQuestion){
-                console.log('You are finished');
-                $("#forms").hide();
-                message.textContent = 'Finish';
-                // location.reload();
-                setTimeout(location.reload.bind(location), 3000); // After 5 seconds the page will reload
-                return;
-            }
-            loadQuestion(currentQuestion);
-            
-        });
-    
-  
-        
-    } // end of the loadNextQuestion function
+      selectedOption = false;
+      currentQuestion++;
 
+      if (currentQuestion === totalQuestion) {
+        console.log("You are finished");
+        clearTimeout(hideFormTimeout);
 
+        $("#gameMessage").text("Finish");
 
-    $("#start").on('click', function(){
-        
-        $("#start").hide();
-        $("#forms").show();
-        loadQuestion(currentQuestion);
+        setTimeout(() => {
+          console.log("show game message timeout");
+          $("#gameMessage").show();
+        }, 2001);
+        setTimeout(() => {
+          console.log("hide game message timeout");
+          $("#gameMessage").hide();
+        }, 5000);
+        setTimeout(location.reload.bind(location), 5001); // After 5 seconds the page will reload
+        return;
+      }
+      loadQuestion(currentQuestion);
+    });
+  } // end of the loadNextQuestion function
+
+  $("#start").on("click", function() {
+    $("#start").hide();
+    $("#forms").show();
+    loadQuestion(currentQuestion);
     //     var count = 10;
     //     setInterval(function(){
     //         loadQuestion(currentQuestion);
@@ -104,59 +103,18 @@ $(document).ready (function(){
     //         count--;
     //         if (count == 0 && totalQuestion >0){
     //             currentQuestion++;
-                
+
     //             loadNextQuestion();
     //             count=10;
     //             return;
-           
+
     //         }
     // }, 1000); // end od setInterval
-   
-        // 
-        loadNextQuestion();
-    });
 
-    
-
-
+    //
+    loadNextQuestion();
+  });
 });
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // game.questions[0].question;
 // if game.question[0].possibleAnwsers[1] then return true;
@@ -168,8 +126,6 @@ $(document).ready (function(){
 //     var answerChoices = game.possibleAnswers;
 // }
 
-
- 
 // function createContent(questionIndex, contentArea){
 //     var index = questions[questionIndex];
 //     var formsDiv = $("<div class='forms' >");
